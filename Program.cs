b88600488add47c433f37ed99e928a08cc075018
Xurnix (aka Phoenix) <jacklbowen2004@gmail.com>
2026-05-09 12:56:06 -0500

    Improved logging and crash handling

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.IO;
 using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -11,8 +15,9 @@ namespace LocalServer
 
         static readonly string IP_ADDRESS = "127.0.0.1";
         static readonly int[] HTTP_PORTS = { 80, 443 };
-        static readonly string LOG_FILEPATH = "Server.log";
-
+        static readonly string BASE_PATH = AppContext.BaseDirectory;
+        static string LOG_FILEPATH = Path.Combine(BASE_PATH, "Logs", "Server.log");
+        
         // OTHER
 
         static readonly object _logLock = new();
@@ -21,8 +26,10 @@ namespace LocalServer
 
         static async Task Main(string[] args)
         {
-            File.WriteAllText(LOG_FILEPATH, string.Empty);
-
+            RotateLogs();
+            
+            File.AppendAllText(LOG_FILEPATH, $"Ace Combat Infinity: Local Server started at {DateTime.Now}\nRuntime version {RuntimeInformation.FrameworkDescription}\nApplication Directory: {BASE_PATH}\n\n");
+            
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Logging.ClearProviders();
@@ -39,7 +46,16 @@ namespace LocalServer
 
             var app = builder.Build();
 
-            app.Use(async (context, next) =>
+            app.Lifetime.ApplicationStarted.Register(() =>
+            {
+                foreach (int port in HTTP_PORTS)
+                {
+                    Log($"[HTTP {IP_ADDRESS}:{port}] Started listening.", "Info");
+                }
+                Log("All listeners started. Server online.\n", "Info");
+            });
+
+        app.Use(async (context, next) =>
             {
                 await LogRequestAsync(context);
                 await next();
@@ -51,27 +67,88 @@ namespace LocalServer
             //         "data" field and crashes with a null deref otherwise)
             app.MapGet("/{**catchAll}", HandleHttpGetRequest);
             app.MapPost("/{**catchAll}", HandleHttpPostRequest);
-
-            foreach (int port in HTTP_PORTS)
-            {
-                Log($"[HTTP {IP_ADDRESS}:{port}] Started listening.");
-            }
-
-            Log("All listeners started. Server online.\n");
-
+            
             try
             {
                 await app.RunAsync();
             }
             catch (Exception e)
             {
-                Log(e.Message);
+                HandleCrash(e);
             }
 
-            Console.Write("All listeners stopped. Server offline. Press any key to exit...");
-            Console.ReadKey();
+            Log("All listeners stopped.", "Info");
+            Log("Server shutting down...", "Info");
+            File.AppendAllText(LOG_FILEPATH, $"Server shutdown time: {DateTime.Now}, log file closed.");
+            Environment.Exit(0);
         }
 
+        public static void RotateLogs()
+        {
+            if (!Directory.Exists(Path.Combine (BASE_PATH, "Logs")))
+            {
+                Directory.CreateDirectory(Path.Combine(BASE_PATH, "Logs"));
+            }
+
+            if (!Directory.Exists(Path.Combine(BASE_PATH, "Crashes")))
+            {
+                Directory.CreateDirectory(Path.Combine(BASE_PATH, "Crashes"));
+            }
+            // Pretty simple implementation of a log rotation system. Stores them in LOG_FILEPATH, then once a new session starts. The old log is copied and numbered.
+            // The old one is deleted and a new Server.log is generated in it's place.
+            if (!File.Exists(LOG_FILEPATH))
+                return;
+            
+            int LOG_INDEX = 0;
+            string LOG_ARCHIVEPATH;
+            do
+            {
+                LOG_ARCHIVEPATH = Path.Combine (BASE_PATH, "Logs", $"Server_{LOG_INDEX}.log");
+                LOG_INDEX++;
+            } 
+            while (File.Exists(LOG_ARCHIVEPATH));
+            
+            File.Copy(LOG_FILEPATH, LOG_ARCHIVEPATH);
+            File.Delete(LOG_FILEPATH);
+
+        }
+        
+        // Generate useful crash data and diagnostics.
+        public static void HandleCrash(Exception ex)
+        {
+            var CrashDumpPath = Path.Combine(BASE_PATH, "Crashes", $"LocalServerCrash-{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.txt");
+            var OSName = RuntimeInformation.OSDescription;
+            
+            if (ex.Source == "System.Net.Sockets" && RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Log($"Port Binding Failed: Unable to bind to Port 80 (HTTP), Port 443 (HTTPS)", "Error");
+                Log(@"Note to Linux Users:
+On Unix & Unix-Like Operating Systems. Applications not running as root (sudo) cannot bind to Port 80 (HTTP) or Port 443 (HTTPS), these ports are required for Local Server to function.
+Please restart the Ace Combat Infinity Local Server as root (sudo), eg: sudo ./LocalServer" + '\n', "Info");
+            }
+            else if (ex.Source == "System.Net.Sockets")
+            {
+                Log($"Port Binding Failed: Unable to bind to Port 80 (HTTP), Port 443 (HTTPS)", "Error");
+            }
+            Log("Exception occured in main process: "+ ex.Message + " (" + ex.Source + ")", "Error");
+            
+            Console.Write("Writing crashlog to file: " + CrashDumpPath + '\n');
+            
+            var DumpText =
+$@"Crash log generated on {DateTime.Now:yyyy-MM-dd_HH-mm-ss}
+Crash caused by: {ex.Source}, with message: {ex.Message}
+
+Operating System: {OSName}
+Architecture: {RuntimeInformation.OSArchitecture}
+Application Directory: {BASE_PATH}
+Runtime Version: {RuntimeInformation.FrameworkDescription}
+
+Call stack trace:
+{ex.ToString()}";
+            
+            File.AppendAllText(CrashDumpPath, DumpText);
+        }
+        
         static async Task LogRequestAsync(HttpContext context)
         {
             var request = context.Request;
@@ -119,7 +196,7 @@ namespace LocalServer
                 sb.Append('\n');
             }
 
-            Log(sb.ToString());
+            Log(sb.ToString(), "Info");
         }
 
         static async Task HandleHttpGetRequest(HttpContext context)
@@ -128,7 +205,7 @@ namespace LocalServer
                 "##########################################################\n" + 
                 "# HTTP GET REQUEST CALLED - PLEASE REPORT TO OPTIMUS1200 #\n" +
                 "##########################################################\n"
-            );
+            , "Warn");
 
             var response = new JObject
             {
@@ -168,13 +245,35 @@ namespace LocalServer
             await context.Response.Body.WriteAsync(bodyBytes, 0, bodyBytes.Length);
         }
 
-        static void Log(string data)
+        static class LogColors
+        {
+            public const string Reset = "\u001b[0m";
+
+            public const string Info = "\u001b[36m"; 
+            public const string Warn = "\u001b[33m";
+            public const string Error = "\u001b[31m";
+        }
+        static void Log(string data, string level)
         {
             lock (_logLock)
             {
-                Console.WriteLine(data);
-                File.AppendAllText(LOG_FILEPATH, data + '\n');
+                switch (level)
+                {
+                    case "Info":
+                        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd_HH-mm-ss}] {LogColors.Info}[Info] {LogColors.Reset}{data}");
+                        File.AppendAllText(LOG_FILEPATH, $"[{DateTime.Now:yyyy-MM-dd_HH-mm-ss}] [Info] " + data + '\n');
+                        break;
+                    case "Warn":
+                        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd_HH-mm-ss}] {LogColors.Warn}[Warning] {LogColors.Reset}{data}");
+                        File.AppendAllText(LOG_FILEPATH, $"[{DateTime.Now:yyyy-MM-dd_HH-mm-ss}] [Warn] " + data + '\n');
+                        break;
+                    case "Error":
+                        Console.WriteLine($"[{DateTime.Now:yyyy-MM-dd_HH-mm-ss}] {LogColors.Error}[Error] {LogColors.Reset}{data}");
+                        File.AppendAllText(LOG_FILEPATH, $"[{DateTime.Now:yyyy-MM-dd_HH-mm-ss}] [Error] " + data + '\n');
+                        break;
+                }
             }
         }
     }
 }
+
